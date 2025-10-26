8bf5992bc5892afb7129e0cb84a1bf8470d68327
Miguel Hurtado <migue.ahg16@gmail.com>
2025-10-26 10:32:37 -0400

    GIthub Pages Fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/<repo-name>/', // e.g. '/flite/' if this is NOT mmm1602.github.io
 })
